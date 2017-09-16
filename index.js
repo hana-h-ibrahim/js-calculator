2324965218b5b9767f98089b388b07e889501ecf
Hana Hilmy <hanahilmy@users.noreply.github.com>
2017-09-17 00:20:22 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+	var testNumLength = function(number) {
+        if (number.length > 9) {
+            totaldiv.text(number.substr(number.length-9,9));
+            if (number.length > 15) {
+                number = "";
+                totaldiv.text("Err");
+            }
+        } 
+    };
+	var number = "";
+    var newnumber = "";
+    var operator = "";
+    var totaldiv = $("#total");
+    totaldiv.text("0");
+    $("#numbers > a").not("#clear,#clearall").click(function(){
+		number += $(this).text();
+		totaldiv.text(number);
+		testNumLength(number);
+    });
+    $("#operators > a").not("#equals").click(function(){
+		operator = $(this).text();
+		newnumber = number;
+		number = "";
+		totaldiv.text("0");
+    });
+    $("#clearall").click(function(){
+		number = "";
+		totaldiv.text("0");
+	
+			newnumber = "";
+		
+    });
+    //Add your last .click() here!
+       $("#equals").click(function(){
+    	if (operator === "+"){
+    	    number = (parseInt(number, 10) + parseInt(newnumber,10)).toString(10);
+    	} 
+    	else if (operator === "-"){
+    		number = (parseInt(newnumber, 10) - parseInt(number,10)).toString(10);
+    	} 
+    	else if (operator === "/"){
+    		number = (parseInt(newnumber, 10) / parseInt(number,10)).toString(10);
+    	} 
+    	else if (operator === "*"){
+    		number = (parseInt(newnumber, 10) * parseInt(number,10)).toString(10);
+    	}
+    	totaldiv.text(number);
+    	testNumLength(number);
+    	number = "";
+    	newnumber = "";
+    });
+});
